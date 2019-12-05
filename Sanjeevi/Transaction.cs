386c3a171b4edd79ee22e35877f3197d2cd76ad9
Vaduganathan <vaduga@outlook.com>
2019-12-06 01:01:23 +0530

    Init

---
+++ Sanjeevi/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sanjeevi
+{
+    public class Transaction
+    {
+        private string _from { get; set; }
+        private string _to { get; set; }
+        private double _amount { get; set; }
+        
+        public Transaction(string from, string to, double amount)
+        {
+            _from = from;
+            _to = to;
+            _amount = amount;
+        }
+
+        public string From { get { return _from; } }
+        public string To { get { return _to; } }
+        public double Amount { get { return _amount; } }
+    }
+}
